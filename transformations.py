a8a4ffae0d0104e5abc8ac1d4cb2891ab756df3b
adhfmz7 <ahmadaldasouqi559@gmail.com>
2022-03-16 17:41:12 -0700

    commit

--- transformations.py
+++ transformations.py
@@ -1,14 +1,17 @@
-# transforms vectors using matrix multiplication
 
+
+# transforms vector by multiplying by a transformation matrix 
 def matmul(mat1, mat2):
   if len(mat1[0]) != len(mat2):
     raise Exception("matrices can not be multiplied")
   
-
-A = [[1, 2, 3],
+  
+A = [[1, 2, 3], #3x3 matrix
      [4, 5, 6],
      [7, 8, 9]]
 
-B = [[1, 2, 3],
+B = [[1, 2, 3], #2 x 3 matrix
      [6, 5, 4]]
+
+print(matmul(B, A))
 
